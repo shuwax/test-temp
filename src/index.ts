59769c3c5f8b63d12676d3b76f5423ae2806aabb
r_smolonskij <ruslans.smolonskis@gmail.com>
2024-09-26 07:50:22 +0300

    Clean index.js

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,4 @@
-import { AccountAPI } from './AccountAPI/index.js';
-import { ApiService } from './ApiService/index.js';
-import { GroupsAPI } from './GroupsAPI/index.js';
-
-const baseUrl = 'https://api.awsdev.cosafe.se';
-
-const apiService = new ApiService(baseUrl);
-const accountsAPI = new AccountAPI(apiService);
-const groupsAPI = new GroupsAPI(apiService);
-
-const flow = async () => {
-  await accountsAPI.getAntiForgeryToken();
-  await accountsAPI.login({
-    email: 'dev1@example.com',
-    password: 'Cats-45678',
-  });
-  await accountsAPI.logout();
-  // await groupsAPI.getGroupById({ id: 3448 });
-  // await groupsAPI.getGroups({ limit: 2 });
-  // await groupsAPI.getGroupMembers({ id: 3448}, { limit: 2 });
-};
-
-flow();
+export * from './ApiService/index.js';
+export * from './AccountAPI/index.js';
+export * from './GroupsAPI/index.js';
+export * from './LocationsAPI/index.js';
