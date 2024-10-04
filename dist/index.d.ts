1a27a9ec47c768dca21859e0657e4c57a208e1a8
r_smolonskij <ruslans.smolonskis@gmail.com>
2024-10-04 12:41:10 +0300

    Change package name

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -2,3 +2,6 @@ export { ApiService } from './ApiService/index.js';
 export { AccountAPI } from './AccountAPI/index.js';
 export { GroupsAPI } from './GroupsAPI/index.js';
 export { LocationsAPI } from './LocationsAPI/index.js';
+export { OrganizationsAPI } from './OrganizationsAPI/index.js';
+export { UsersAPI } from './UsersAPI/index.js';
+export { AlarmsAPI } from './AlarmsAPI/index.js';
